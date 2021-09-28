38626ea142e5b003216686c91a07db9352dd2350
Юрий <70635903+drunkexorzist@users.noreply.github.com>
2021-09-28 14:32:15 +1000

    1

--- test.py
+++ test.py
@@ -1,1 +1,15 @@
-
+lst=[]
+lst1=[]
+lst2=[]
+for i in range(6):
+    a=input()
+    lst.append(a)
+for j in range(6):
+    if j%2==0:
+        lst1.append(lst[j])
+    else:
+        lst2.append(lst[j])
+str1=', '.join(lst1)
+str2=', '.join(lst2)
+print(str1)
+print(str2)
