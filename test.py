a9ee919f3a04835e14cfb6f67d2e3a9fc96be37b
Юрий <70635903+drunkexorzist@users.noreply.github.com>
2021-09-30 08:57:24 +1000

    1

--- test.py
+++ test.py
@@ -1,15 +1,4 @@
+import numpy as n
 lst = list(map(int, input().split()))
 
-lst_max = max(lst)
-lst_min = min(lst)
-
-lst_out = []
-
-for i in lst:
-    a = lst.index(i)
-    if a == lst.index(lst_max):
-        lst_out.append(lst_min)
-    else:
-        lst_out.append(i)
-        
-print(*lst_out)
+print(lst)
