43acdc0ff741ffa6712a5373159fdb87d96599ff
Юрий <70635903+drunkexorzist@users.noreply.github.com>
2021-10-19 09:44:47 +1000

    Update D.sql

--- sql/D.sql
+++ sql/D.sql
@@ -1,20 +1,21 @@
 SELECT
-	Employees.fullname as EmployeeFullNames,
-    sum(Positions.Salary * Salaries.SalaryPercentage) as SumSalary
-from Employees
-	left JOIN Salaries on Salaries.EmployeeID = Employees.ID
-    left JOIN Positions on Salaries.PositionID = Positions.ID
-WHERE Positions.Salary > 0 
-AND Salaries.SalaryPercentage BETWEEN 0 AND 1
-GROUP BY Employees.fullname
-HAVING SumSalary <(
-	SELECT avg(avg_sum)
-	FROM (
-    	SELECT 
-    		sum(Salaries.SalaryPercentage * Positions.Salary) as avg_sum
-		from Salaries
-			LEFT JOIN Employees on Salaries.EmployeeID = Employees.ID
-    		LEFT JOIN Positions on Salaries.PositionID = Positions.ID
-		GROUP BY Employees.FullName
-    ) 
+	Employees.FullName as EmployeeFullNames ,
+    round(sum(Salaries.SalaryPercentage * Positions.Salary),0) as SumSalary
+FROM Salaries
+	JOIN Employees on Salaries.EmployeeID = Employees.ID
+    JOIN Positions on Salaries.PositionID = Positions.ID
+GROUP by Employees.FullName
+HAVING SumSalary < (
+	SELECT 
+		avg(rab_sum)
+	from(
+		SELECT
+			Employees.FullName,
+    		sum(Salaries.SalaryPercentage * Positions.Salary) as rab_sum
+		FROM Salaries
+			JOIN Employees on Salaries.EmployeeID = Employees.ID
+    		JOIN Positions on Salaries.PositionID = Positions.ID
+		GROUP by Employees.FullName
 )
+  )
+ORDER by SumSalary
